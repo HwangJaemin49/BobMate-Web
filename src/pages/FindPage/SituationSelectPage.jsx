1f93d27f04b3e733eb3fb071c2f9102677688d63
yiksan0315 <none>
2024-01-08 22:59:33 +0900

    third page: select-content

--- src/pages/FindPage/SituationSelectPage.jsx
+++ src/pages/FindPage/SituationSelectPage.jsx
@@ -18,10 +18,10 @@ const SituationPage = () => {
       <FindPageWrapper step='1단계' prevLink='/' nextLink={nextLink}>
         <Title>{'식사 상황을 선택해 주세요!'}</Title>
         <MaxWidthWrapper className='flex flex-wrap justify-between'>
-          <RoundButton className='w-48 h-48 lg:w-52 lg:h-52' title='일반 상황'>
+          <RoundButton className='w-48 h-48 lg:w-56 lg:h-56' title='일반 상황'>
             {'내 상황을 직접 설명할께요'}
           </RoundButton>
-          <RoundButton className='w-48 h-48 lg:w-52 lg:h-52' title='특정 상황'>
+          <RoundButton className='w-48 h-48 lg:w-56 lg:h-56' title='특정 상황'>
             {"'밥 친구'가 알려주는 상황으로 선택할께요"}
           </RoundButton>
         </MaxWidthWrapper>
