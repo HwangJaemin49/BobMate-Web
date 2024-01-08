a92a838a45b2f247cd66d4d2a13085de953c2a97
yiksan0315 <none>
2024-01-08 20:55:00 +0900

    tailwind css config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import SituationPage from './pages/FindPage/SituationPage';
 
 function App() {
   return (
-    <div className="root-wrap">
+    <div className='root-wrap'>
       <BrowserRouter>
-        <div id = "wrapper">
+        <div id='wrapper'>
           <Header />
-          <div id = "main-content">
+          <div id='main-content'>
             <Routes>
-
+              <Route
+                path='/find-page'
+                element={<SituationPage></SituationPage>}
+              />
             </Routes>
           </div>
         </div>
-
       </BrowserRouter>
     </div>
-
   );
 }
 
---
+++ src/pages/FindPage/SituationPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SituationPage = () => {
+  return <div className='text-blue-500'>tailwind css test</div>;
+};
+
+export default SituationPage;
