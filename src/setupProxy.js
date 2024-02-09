7ff7fa48cbb0d112c5f396367b3b9537e0f3875c
yiksan0315 <1889sanik1889@gmail.com>
2024-02-09 23:07:16 +0900

    proxy 주소 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api/v1', {
-      // target: 'http://43.202.23.75/',
-      target: 'http://localhost:8080',
+      target: 'http://43.202.23.75/',
       changeOrigin: true,
     })
   );
