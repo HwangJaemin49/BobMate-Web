b1c74ab6d89171c3549cc0e787af0b573dc25b71
HwangJaemin49 <ch06159@gmail.com>
2024-02-15 23:58:01 +0900

    세부사항 수정

--- src/components/Recommend.jsx
+++ src/components/Recommend.jsx
@@ -6,12 +6,12 @@ export default function Content({ results }) {
         <div className='content-container' style={{ margin: "10px auto", height: "350px", textAlign: "center", width: "1000px", fontFamily: "Pretendard-SemiBold" }}>
             {results.map((item, index) => (
                 <div key={index} className='content-box' style={{ height: "350px", float: index === 0 ? 'left' : 'right', width: "30%", marginLeft: index === 1 ? "5%" : "0", boxSizing: "border-box", borderRadius: "30px" }}>
-                    <img src={item.imgUrl} style={{ backgroundColor: "#D9D9D9", height: "300px", width: "300px", borderRadius: "50%" }} alt={`Content ${index + 1}`}></img>
+                    <img src={item.imgUrl} style={{ backgroundColor: "#D9D9D9", height: "300px", width: "300px", borderRadius: "50%", border: "2px solid #E9E9E9"}} alt={`Content ${index + 1}`}></img>
                     <div className='content-information' style={{ height: '30px' }}><br />
                         <span style={{ fontSize: "24px" }}>{item.name}</span>
                     </div>
                     <Link to={item.linkUrl} target="_blank" rel="noopener noreferrer">
-                        <div className='button' style={{ width: "100px", border: "2px solid", fontSize: "20px", borderRadius: "10px", marginLeft: "90px", marginTop: "60px" }}>보러 가기</div>
+                        <div className='button' style={{ width: "140px", border: "2px solid", fontSize: "20px", borderRadius: "10px", marginLeft: "90px", marginTop: "60px", height: "40px", paddingTop:"5px"}}>보러 가기</div>
                     </Link>
                 </div>
             ))}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
     }
 
     return(
-    <div style={{textAlign:"center"}}>
+    <div style={{textAlign:"center", height: "450px"}}>
         <div className="login" style={{marginTop: "100px"}}>
             <p style={{fontSize: "36px"}}>로그인</p><br /><br />
             <p style={{fontSize: "18px"}}>간편 로그인으로 바로 시작해 보세요!</p><br />
