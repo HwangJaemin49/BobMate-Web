7229197311148a090094f5e4fc125dad273cad7f
alex-koko <bookshelf1234@ewhain.net>
2024-02-19 20:38:58 +0900

    카카오

--- src/components/KakaoLogin.js
+++ src/components/KakaoLogin.js
@@ -1,56 +1,51 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { setToken } from '../store/tokenState';
 
+function KakaoLogin({ setAccessToken }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const PARAMS = new URL(document.location).searchParams;
+  const KAKAO_CODE = PARAMS.get('code');
+  const [accessTokenFetching, setAccessTokenFetching] = useState(false);
 
-function KakaoLogin({setAccessToken}) {
-    const navigate = useNavigate();
-    const PARAMS = new URL(document.location).searchParams;
-    const KAKAO_CODE = PARAMS.get("code");
-    const [accessTokenFetching, setAccessTokenFetching] = useState(false);
+  const getAccessToken = async () => {
+    if (accessTokenFetching || !KAKAO_CODE) return;
+    try {
+      setAccessTokenFetching(true);
 
-
-    const getAccessToken = async () => {
-        if (accessTokenFetching || !KAKAO_CODE) return;
-
-
-        try {
-            setAccessTokenFetching(true);
-
-            const response = await axios.post(
-                `http://43.202.23.75/api/v1/auth/kakao`,
-                {
-                    "authorizationCode": `${KAKAO_CODE}`,
-                },
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
-
-            const accessToken = response.data.result.accessToken;
-            localStorage.setItem('accessToken', accessToken);
-            setAccessToken(accessToken);
-
-            setAccessTokenFetching(false);
-            navigate("/");
-        } catch (error) {
-            console.error("Error ", error);
-        } finally {
-            setAccessTokenFetching(false);
+      const response = await axios.post(
+        `http://43.202.23.75/api/v1/auth/kakao`,
+        {
+          authorizationCode: `${KAKAO_CODE}`,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
         }
-    };
+      );
 
-    useEffect(() => {
-        getAccessToken();
-    }, [KAKAO_CODE, setAccessToken]);
+      const accessToken = response.data.result.accessToken;
+      localStorage.setItem('accessToken', accessToken);
+      setAccessToken(accessToken);
+      dispatch(setToken(accessToken));
 
-    return (
-        <div>
-            Loading...
-        </div>
-    );
+      setAccessTokenFetching(false);
+      navigate('/');
+    } catch (error) {
+      console.error('Error ', error);
+    } finally {
+      setAccessTokenFetching(false);
+    }
+  };
+
+  useEffect(() => {
+    getAccessToken();
+  }, [KAKAO_CODE, setAccessToken]);
+
 }
 
 export default KakaoLogin;
