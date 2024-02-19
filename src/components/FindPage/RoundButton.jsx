b4f73a3c020a3f17f594226f0fb6515fb368dc22
yiksan0315 <1889sanik1889@gmail.com>
2024-02-19 16:39:20 +0900

    버튼 클릭 시 색상 변화

--- src/components/FindPage/RoundButton.jsx
+++ src/components/FindPage/RoundButton.jsx
@@ -4,7 +4,7 @@ const RoundButton = ({ onClick, value, isSelected, children, className }) => {
   return (
     <button
       className={classnames(
-        'rounded-full bg-white flex flex-col items-center justify-center text-center   border-grayscale-800 border-2 ',
+        'rounded-full  flex flex-col items-center justify-center text-center   border-grayscale-800 border-2 ',
         'hover:scale-105 hover:bg-grayscale-100  transition-transform ease-in-out duration-200 ',
         { 'scale-105 bg-grayscale-300': isSelected },
         className
--- src/components/FindPage/Situation/SituationButton.jsx
+++ src/components/FindPage/Situation/SituationButton.jsx
@@ -13,7 +13,7 @@ const SituationButton = ({
   return (
     <button
       className={classNames(
-        'px-9 py-6 text-center border-2 rounded-full border-grayscale-800 bg-white',
+        'px-9 py-6 text-center border-2 rounded-full border-grayscale-800 ',
         {
           'hover:scale-105 hover:bg-grayscale-100  transition-transform ease-in-out duration-200 ':
             !disabled,
